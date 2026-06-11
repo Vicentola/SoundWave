32158ed76c39e2c9bc8315da9e8f26ca35ec9c42
Vicentola <135228454+Vicentola@users.noreply.github.com>
2026-06-11 21:37:24 +0000

    fim dos patterns

--- patterns.py
+++ patterns.py
@@ -1,3 +1,8 @@
+import random
+
+
+
+
 class PlayerManager :
 
     # Singleton — garante apenas uma instância do player
@@ -39,8 +44,53 @@ class EventBus :
     def emit(self, event, data):
         if event in self._listeners:
             for callback in self._listeners[event]:
-                funcao(data)
+                callback(data)
 
+
+class Command:
+
+    def execute(self):
+        pass
+
+
+    def undo(self):
+        pass
+
+
+class VolumeCommand(Command):
+
+    def __init__(self, player, volume):
+        self.player = player
+        self.volume = volume
+        self.prev_volume = None
+
+    def execute(self):
+        self.prev_volume = self.player.volume
+        self.player.volume = self.volume
+
+    def undo(self):
+        self.player.volume = self.prev_volume
+
+
+class PlaybackStrategy:
+    def get_next(self, queue):
+        pass
+
+class NormalStrategy(PlaybackStrategy):
+# vai fazer a execução de fila normal
+    def get_next(self, queue):
+        if queue:
+            return queue.pop(0)
+        return None
+
+class ShuffleStrategy(PlaybackStrategy):
+
+    def get_next(self, queue):
+        if queue:
+            song = random.choice(queue)
+            queue.remove(song)
+            return song
+        return None
 
 
 
@@ -48,8 +98,32 @@ class EventBus :
 
 
 if __name__ == "__main__":
+
+
     p1 = PlayerManager()
     p2 = PlayerManager()
     print(p1 is p2)  # deve printar True
     p1.volume = 0.5
     print(p2.volume)  # deve printar 0.5 — são o mesmo objeto!
+    bus = EventBus()    
+    bus.subscribe("play", lambda data: print(f"Tocando: {data}"))
+    bus.subscribe("play", lambda data: print(f"Atualizando interface: {data}"))
+    bus.emit("play", "Bohemian Rhapsody")
+    bus.emit("stop", "nada") 
+    player = PlayerManager()
+    print(f"Volume inicial: {player.volume}")
+
+    cmd = VolumeCommand(player, 0.3)
+    cmd.execute()
+    print(f"Volume após execute: {player.volume}")
+
+    cmd.undo()
+    print(f"Volume após undo: {player.volume}")
+    queue = ["Bohemian Rhapsody", "Billie Jean", "Creep", "Superstition"]
+
+    normal = NormalStrategy()
+    print(normal.get_next(queue))  # sempre o primeiro
+
+    shuffle = ShuffleStrategy()
+    print(shuffle.get_next(queue))  # aleatório
+    print(queue)  # deve ter 2 músicas — uma foi removida pelo normal, outra pelo shuffle
